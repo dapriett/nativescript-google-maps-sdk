b4f33a8c7347a36a03d2156040102687255ce34f
Dan Prietti <dprietti@datainterfuse.com>
2015-10-21 11:47:41 -0400

    Cleanup

--- map-view-common.js
+++ map-view-common.js
@@ -2,6 +2,9 @@ var view = require("ui/core/view");
 var dObservable = require("ui/core/dependency-observable");
 var proxy = require("ui/core/proxy");
 
+var MAPVIEW = "MapView";
+var CAMERA_PROPERTIES = [ "latitude", "longitude", "bearing", "zoom", "tilt" ];
+
 var MapView = (function (_super) {
   global.__extends(MapView, _super);
   function MapView() {
@@ -22,19 +25,14 @@ var MapView = (function (_super) {
 
   MapView.mapReadyEvent = "mapReady";
 
-  var properties = [ "latitude", "longitude", "bearing", "zoom", "tilt" ];
-  properties.forEach(function( name ) {
-    var property = new dObservable.Property(
-        name,
-        "MapView",
-        new dObservable.PropertyMetadata(
-            0,
-            dObservable.PropertyMetadataSettings.None,
-            function( data ) {
-              data.object.updateCamera();
-            }
-        )
-    );
+  var onCameraPropertiesChanged = function(data) {
+    var mapView = data.object.updateCamera();
+    mapView.updateCamera(data);
+  }
+
+  CAMERA_PROPERTIES.forEach(function( name ) {
+    var metadata = new dObservable.PropertyMetadata(0, dObservable.PropertyMetadataSettings.None, onCameraPropertiesChanged);
+    var property = new dObservable.Property(name, MAPVIEW, metadata);
     exports[ name + "Property" ] = property;
 
     Object.defineProperty( MapView.prototype, name, {
