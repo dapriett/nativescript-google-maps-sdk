e86f0a7820735aabd409206111c80037aee0a25a
Dirk Rudolph <dirk.rudolph@netcentric.biz>
2017-07-01 19:15:05 +0200

    made the custom icon image compatible to tns-core-modules@3.1.0

--- map-view.android.ts
+++ map-view.android.ts
@@ -10,6 +10,7 @@ import {
     tiltProperty, StyleBase, UISettingsBase, getColorHue
 } from "./map-view-common";
 import { Image } from "tns-core-modules/ui/image";
+import { ImageSource } from "image-source";
 import { Color } from "tns-core-modules/color";
 import { Point } from "tns-core-modules/ui/core/view";
 import imageSource = require("tns-core-modules/image-source");
@@ -590,7 +591,7 @@ export class Bounds extends BoundsBase {
 export class Marker extends MarkerBase {
     private _android: any;
     private _color: number;
-    private _icon: Image;
+    private _icon: ImageSource;
     private _isMarker: boolean = false;
 
     static CLASS = 'com.google.android.gms.maps.model.Marker';
@@ -694,14 +695,12 @@ export class Marker extends MarkerBase {
         return this._icon;
     }
 
-    set icon(value: Image|string) {
+    set icon(value: ImageSource|string) {
         if (typeof value === 'string') {
-            var tempIcon = new Image();
-            tempIcon.imageSource = imageSource.fromResource(String(value));
-            value = tempIcon;
+            value = imageSource.fromResource(String(value));
         }
         this._icon = value;
-        var androidIcon = (value) ? com.google.android.gms.maps.model.BitmapDescriptorFactory.fromBitmap(value.imageSource.android) : null;
+        var androidIcon = (value) ? com.google.android.gms.maps.model.BitmapDescriptorFactory.fromBitmap(value.android) : null;
         if (this._isMarker) {
             this._android.setIcon(androidIcon);
         } else {
