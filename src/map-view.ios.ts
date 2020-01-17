454fdec90c92f200bdc35cf196f3fd10d3b48bb2
mikkelmoerch <mikmo334@gmail.com>
2020-01-17 12:17:37 +0100

    feat(ios): set ios nativeview in view lifecycle #398

--- src/map-view.ios.ts
+++ src/map-view.ios.ts
@@ -278,19 +278,11 @@ class MapViewDelegateImpl extends NSObject implements GMSMapViewDelegate {
 
 export class MapView extends MapViewBase {
 
+    public nativeView: GMSMapView;
     protected _markers: Array<Marker> = new Array<Marker>();
 
     private _delegate: MapViewDelegateImpl;
     private _indoorDelegate:IndoorDisplayDelegateImpl;
-
-    constructor() {
-        super();
-
-        this.nativeView = GMSMapView.mapWithFrameCamera(CGRectZero, this._createCameraPosition());
-        this._delegate = MapViewDelegateImpl.initWithOwner(new WeakRef(this));
-        this._indoorDelegate = IndoorDisplayDelegateImpl.initWithOwner(new WeakRef(this));
-        this.updatePadding();
-    }
 
     public onLoaded() {
         super.onLoaded();
@@ -313,6 +305,20 @@ export class MapView extends MapViewBase {
         GC();
     };
 
+    public createNativeView() {
+        const mapView = GMSMapView.mapWithFrameCamera(CGRectZero, this._createCameraPosition());
+        this._delegate = MapViewDelegateImpl.initWithOwner(new WeakRef(this));
+        this._indoorDelegate = IndoorDisplayDelegateImpl.initWithOwner(new WeakRef(this));
+        this.updatePadding();
+
+        return mapView;
+    }
+
+    public initNativeView() {
+        (<any>this.nativeView).owner = this;
+        super.initNativeView();
+    }
+
     private _createCameraPosition() {
         return GMSCameraPosition.cameraWithLatitudeLongitudeZoomBearingViewingAngle(
             this.latitude,
@@ -324,10 +330,12 @@ export class MapView extends MapViewBase {
     }
 
     updateCamera() {
-        if (this.mapAnimationsEnabled) {
-            this.nativeView.animateToCameraPosition(this._createCameraPosition());
-        } else {
-            this.nativeView.camera = this._createCameraPosition();
+        if (this.nativeView) {
+            if (this.mapAnimationsEnabled) {
+                this.nativeView.animateToCameraPosition(this._createCameraPosition());
+            } else {
+                this.nativeView.camera = this._createCameraPosition();
+            }
         }
     }
 
