446228a98444f42ea9e746716d9755fcea1d0b4a
kefah@Cocarz <kefah.bader@gmail.com>
2020-10-27 13:55:59 +0100

    fix: cleanup

--- demo/app/bundle-config.js
+++
@@ -1,7 +0,0 @@
-if (global.TNS_WEBPACK) {
-    //registers tns-core-modules UI framework modules
-    require("bundle-entry-points");
-
-    //register application modules
-    global.registerModule("main-page", function () { return require("./main-page"); });
-}
